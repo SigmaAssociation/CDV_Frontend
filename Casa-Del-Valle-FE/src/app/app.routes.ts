d47129d2e135393ac46b89e84e481491c33c8350
Joca <jch8369065@gmail.com>
2026-09-01 23:41:42 -0600

    Se agregaron guias y un pequeño ejemplo

--- Casa-Del-Valle-FE/src/app/app.routes.ts
+++ Casa-Del-Valle-FE/src/app/app.routes.ts
@@ -1,3 +1,28 @@
 import { Routes } from '@angular/router';
+import { StartPage } from './pages/start-page/start-page';
+import { UserStoryGuidePage } from './pages/user-story-guide-page/user-story-guide-page';
+import { ModelGuidePage } from './pages/model-guide-page/model-guide-page';
+import { DesignGuidePage } from './pages/design-guide-page/design-guide-page';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'guide/user-story',
+        component: UserStoryGuidePage
+    },
+    {
+        path: 'guide/model',
+        component: ModelGuidePage
+    },
+    {
+        path: 'guide/design',
+        component: DesignGuidePage
+    },
+    {
+        path: '',
+        component: StartPage,
+    },
+    {
+        path: '**',
+        redirectTo: ''
+    }
+];
