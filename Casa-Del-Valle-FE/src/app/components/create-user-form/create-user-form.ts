dfd2b1dd542942ff57ce54631a0dd2e6b88e49a3
Ronaldo Hilario <ronaldohilario901000@gmail.com>
2026-09-04 00:47:52 -0600

    implementacion de creacion de usuarios en el frontend

---
+++ Casa-Del-Valle-FE/src/app/components/create-user-form/create-user-form.ts
@@ -0,0 +1,108 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { UserRequest } from '../../models/create-user';
+import { UserService } from '../../services/user.service';
+
+@Component({
+  standalone: true,
+  selector: 'app-create-user-form',
+  imports: [ReactiveFormsModule],
+  templateUrl: './create-user-form.html',
+  styleUrl: './create-user-form.css',
+})
+export class CreateUserForm implements OnInit {
+  userForm!: FormGroup;
+  isCreated: boolean = false;
+  isError: boolean = false;
+  errorMessage:string = '';
+
+  private nameRegex = /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/;
+  private phoneRegex = /^[23456789]\d{7}$/;
+  private addressRegex = /^[a-zA-Z0-9áéíóúÁÉÍÓÚñÑ\s,.\-#]+$/;
+  private dpiRegex = /^\d{13}$/;
+  private emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private userService: UserService
+  ) { }
+
+  ngOnInit(): void {
+    this.userForm = this.formBuilder.group({
+      name: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(2),
+          Validators.maxLength(150),
+          Validators.pattern(this.nameRegex)
+        ]
+      ],
+      phone: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.phoneRegex)
+        ]
+      ],
+      address: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(5),
+          Validators.maxLength(255),
+          Validators.pattern(this.addressRegex)
+        ]
+      ],
+      dpi: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern(this.dpiRegex)
+        ]
+      ],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.maxLength(150),
+          Validators.pattern(this.emailRegex)
+        ]
+      ],
+      password: [
+        '',
+        [
+          Validators.required,
+          Validators.minLength(8),
+          Validators.maxLength(255)
+        ]
+      ],
+      id_role: [1, Validators.required]
+    });
+  }
+
+  isInvalid(controlName: string): boolean {
+    const control = this.userForm.get(controlName);
+    return !!(control && control.invalid && (control.dirty || control.touched));
+  }
+
+  create(): void {
+    this.isCreated = true;
+    return
+    if (this.userForm.valid) {
+      const newUser = this.userForm.value as UserRequest;
+
+      this.userService.createUser(newUser).subscribe({
+        next:() =>{
+          this.isCreated = true;
+        },
+        error:(error) =>{
+          this.isError = true;
+          this.errorMessage = error.error;
+        }
+      });
+    
+    }
+  }
+
+}
---
+++ Casa-Del-Valle-FE/src/app/models/create-user.ts
@@ -0,0 +1,9 @@
+export interface UserRequest {
+  name: string;
+  phone: string;
+  address: string;
+  dpi: string;
+  email: string;
+  password: string;
+  id_role: number;
+}
